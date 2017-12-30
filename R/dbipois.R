78747118b8d902d5e78d84fa16c1449862ed225c
stanyip <stanyip101@gmail.com>
2017-12-30 23:53:09 +0800

    Add files via upload
    faster calculation in dbipois and handling extreme value better

--- R/dbipois.R
+++ R/dbipois.R
@@ -1,8 +1,10 @@
 dbipois <- function(x,y,lambda1,lambda2,lambda3=0,log=FALSE) {
   dat = rbind(x,y)
   dat = split(dat, rep(1:ncol(dat), each = nrow(dat)))
-  u = exp(-(lambda1+lambda2+lambda3))*lambda1^x*lambda2^y/factorial(x)/factorial(y)
-  v = u * sapply(dat, function(z) {u1=min(z); sum(sapply(0:u1,function(k) choose(z[1],k)*choose(z[2],k)*factorial(k)*(lambda3/lambda1/lambda2)^k))})
-  if (log) return(log(v))
-  v
+  if (!lambda3) if (!log) return(dpois(x,lambda1) * dpois(y,lambda2)) else return(dpois(x,lambda1, log=T) + dpois(y,lambda2, log=T))
+  u = (-(lambda1+lambda2+lambda3))+x*log(lambda1)+y*log(lambda2) - lfactorial(x) - lfactorial(y)
+  v = sapply(dat, function(z) {u1=min(z); sum(sapply(0:u1,function(k) exp(lchoose(z[1],k)+lchoose(z[2],k)+lfactorial(k)+k*(log(lambda3)-log(lambda1)-log(lambda2)))))})
+  v = ifelse(v==Inf,0,v)
+  if (log) return(u*log(v))
+  exp(u) * v
 }
